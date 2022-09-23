cb8f9ac35952b61485c2e6015cf1a1f5fd7c4f98
irisu01 <nekomarisakirisame@gmail.com>
2022-09-23 11:12:59 +0000

    eslint fixes

--- events/ready.ts
+++ events/ready.ts
@@ -1,7 +1,7 @@
 import { Serval } from "../src/Serval";
 
 export default async (client: Serval) => {
-    let localization = client.intl.get("en");
+    const localization = client.intl.get("en");
 
-    console.log(localization?.handle("events.ready.message", "Logged in as {0}!", [client.user?.tag!]))
-}
+    console.log(localization?.handle("events.ready.message", "Logged in as {0}!", [client.user?.tag || "e"]));
+};
